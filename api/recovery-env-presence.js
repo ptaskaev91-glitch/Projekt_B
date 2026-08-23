2eafbee7aea53c5c2606ae5f898d9aed8d1c162f
ptaskaev91-glitch <p.taskaev91@gmail.com>
2026-08-24 02:50:18 +0300

    Add temporary recovery env presence probe

---
+++ api/recovery-env-presence.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('cache-control', 'no-store');
+  const names = Object.keys(process.env)
+    .filter(k => /(SSH|VPS|TIMEWEB|TWC|CLOUD|SERVER|BYPASS)/i.test(k))
+    .sort();
+  return res.status(200).json({ ok: true, names });
+}
